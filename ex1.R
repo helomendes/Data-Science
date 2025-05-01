e2be80b4ca70e641a7e2e7c15a6b40671150fa7b
helomendes <heloisabmendes@gmail.com>
2025-05-01 18:23:15 -0300

    R basics

---
+++ ex1.R
@@ -0,0 +1,20 @@
+## what is the sum of the first 100 positive integers?
+
+n <- 1000
+
+sum <- n*(n+1)/2
+
+print(sum)
+
+x <- seq(1, n)
+x
+
+three = sum(seq(1, n))
+print(three)
+
+four = log(sqrt(100), base=10)
+print(four)
+
+n <- 10
+log(exp(n))
+
---
+++ functions.R
@@ -0,0 +1,24 @@
+ls()
+log(8)
+log(a)
+exp(1)
+log(exp(1))
+
+help("log")
+?log
+
+log(10, base=2)
+args(log)
+log(8, base=2)
+
+log(8,2)
+2^3
+help("+")
+
+data()
+CO2
+
+pi
+Inf
+
+# COMENTS
